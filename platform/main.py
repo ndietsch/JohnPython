473afe8c515f95cd993accb80ee7aa3f3ffca8c5
Nathan Dietsch <njd@ndietsch.com>
2020-03-22 19:27:09 -0500

    check in old code

--- platform/main.py
+++ platform/main.py
@@ -16,9 +16,11 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
 
     def new(self):
         # start a new game
+        self.score = 0
         self.all_sprites = pg.sprite.Group()
         self.platforms  = pg.sprite.Group()
         self.player = Player(self)
@@ -47,13 +49,22 @@ class Game:
                 self.player.pos.y = hits[0].rect.top
                 self.player.vel.y = 0
 
-            # If player reaches the top 1/4 of the screen
+        # If player reaches the top 1/4 of the screen
         if self.player.rect.top <= HEIGHT / 4:
             self.player.pos.y += abs(self.player.vel.y)
             for plat in self.platforms:
                 plat.rect.y += abs(self.player.vel.y)
                 if plat.rect.top >= HEIGHT:
                     plat.kill()
+                    self.score += 10
+        # Die !
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in  self.all_sprites:
+                sprite.rect.y -= max(self.player.vel.y,10)
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+            if len(self.platforms) == 0:
+                self.playing = False
 
         while len(self.platforms) < 6:
             width = random.randrange(50, 100)
@@ -76,18 +87,47 @@ class Game:
 
     def draw(self):
         # Game Loop - draw
-        self.screen.fill(BLACK)
+        self.screen.fill(BGCOLOR)
         self.all_sprites.draw(self.screen)
+        self.draw_text(str(self.score),22, WHITE, WIDTH / 2, 15)
         # *after* drawing everything, flip the display
         pg.display.flip()
 
     def show_start_screen(self):
         # game splash/start screen
-        pass
+        self.screen.fill(BGCOLOR)
+        self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT /4)
+        self.draw_text("Arrows to move, space to jump", 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press a key to play", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4 )
+        pg.display.flip()
+        self.wait_for_key()
 
     def show_go_screen(self):
         # game over/continue
         pass
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    waiting = False
+
+
+
+
+
+    def draw_text(self,text,size,color,x,y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text,True,color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface,text_rect)
+
 
 g = Game()
 g.show_start_screen()
